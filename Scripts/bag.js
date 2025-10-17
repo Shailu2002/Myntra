0f9b3a00050931e2b90a318862b06b22db786406
Shailu2002 <shailusingh955960@gmail.com>
2025-10-17 14:31:42 +0530

    complete

--- Scripts/bag.js
+++ Scripts/bag.js
@@ -1,9 +1,59 @@
 let bagitemsobjects = [];
 function onLoad()
 {
-    loadBagitemsobject();
+  loadBagitemsobject();
   displayBagItems();
   displaybagIcon();
+  displayBagsummary();
+}
+function displayBagsummary()
+{
+  let bagsummary = document.querySelector('.bag-summary');
+  if (bagitemsobjects.length != 0) {
+    let totalMRp = 0;
+    let DisMrp = 0;
+    let Convenience_Fee = 99;
+    bagitemsobjects.forEach((item) => {
+      totalMRp += item.original_price;
+      DisMrp += item.original_price - item.current_price;
+    });
+    let tot_amount = totalMRp - DisMrp + Convenience_Fee;
+    bagsummary.innerHTML = `
+             <div class="bag-details-container">
+              <div class="price-header">Price details (${bagitems.length} item)</div>
+              <hr/>
+              <div class="price-item">
+              <span class="price-item-tag">Total MRP</span>
+              <span class="price-item-value"> ₹ ${totalMRp}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Discount on MRP</span>
+              <span class="price-item-value priceDetail-base-discount"> ₹ ${DisMrp}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Convenience Fee</span>
+              <span class="price-item-value"> ₹ ${Convenience_Fee}</span>
+            </div>
+               <hr/>
+              <div class="price-footer">
+              <span class="price-item-tag">Total Amount</span>
+              <span class="price-item-value"> ₹ ${tot_amount}</span>
+            </div>
+             </div> 
+             <!-- bag details ends here -->
+            <button class="btn-place-order">
+            <div >PLACE ORDER</div>
+          </button>
+          `;
+  }
+  else {
+    let bagpage = document.querySelector('.bag-page');
+    bagpage.innerHTML = `<div class="no-item">
+          <img class="img-no-item" src="./images/myntraitem.png" alt="no items!">
+          <h3 class="heading-no-item">Hey,it feels so light!</h3>
+          <p class="content-no-item">There is nothing in your bag.Let's add some items.</p>
+         </div>`;
+  }
 }
 function loadBagitemsobject()
 {
@@ -71,6 +121,6 @@ function removefrombag(itemid)
             return false;
         }
     });
-    localStorage.setItem("bagitems", JSON.stringify(bagitems));
-    onLoad();
+  localStorage.setItem("bagitems", JSON.stringify(bagitems));
+  onLoad();
 }
